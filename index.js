f32eebe49400199568f222c39534018061b047cc
Masud8851 <masud1971.cse@gmail.com>
2021-10-30 01:30:04 +0600

    Server

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
 const express = require("express");
-const { MongoClient } = require("mongodb").MongoClient;
 const cors = require("cors");
-
-const ObjectId = require("mongodb").ObjectId;
-
-const app = express();
-const port = process.env.PORT || 5000;
 require("dotenv").config();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ed7sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+async function run() {
+	try {
+		await client.connect();
+		const assignmentDB = client.db("assignmentDB");
+		const packageCollection = assignmentDB.collection("packages");
+		const OrderCollection = assignmentDB.collection("orders");
+
+		// Add Packeges
+		app.post("/packages", async (req, res) => {
+			const result = await packageCollection.insertOne(req.body);
+			res.send(result);
+			console.log(result);
+		});
+
+		// Get All Packeges
+		app.get("/allpakages", async (req, res) => {
+			const result = await packageCollection.find({}).toArray();
+			res.send(result);
+			console.log("result");
+		});
+	} finally {
+		// await client.close()
+	}
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-	res.send("Server Running");
+	res.send("Hello World!");
 });
 
 app.listen(port, () => {
-	console.log("Server Port", port);
+	console.log(`Example app listening at http://localhost:${port}`);
 });
