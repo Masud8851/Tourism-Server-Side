74371da74eefbdd90117612ce0cae4a08f6ff80b
Masud8851 <masud1971.cse@gmail.com>
2021-10-30 20:35:57 +0600

    Add

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ed7sj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zkd0m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
@@ -20,22 +21,98 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		await client.connect();
-		const assignmentDB = client.db("assignmentDB");
-		const packageCollection = assignmentDB.collection("packages");
-		const OrderCollection = assignmentDB.collection("orders");
+		const porijanDb = client.db("Tourism");
+		const pakageCollection = porijanDb.collection("pakages");
+		const OrderCollection = porijanDb.collection("orders");
+
+		// Booking
+		app.post("/addorder", async (req, res) => {
+			const result = await OrderCollection.insertOne(req.body);
+			res.send(result);
+			console.log("One Order added Successfully");
+		});
+
+		// Insert a myBookings
+		app.post("/myBooking", async (req, res) => {
+			const result = await OrderCollection.insertOne(req.body);
+			res.send(result);
+			console.log("One myBooking added Successfully");
+		});
+
+		// Get My myBookings
+		app.get("/myBooking/:email", async (req, res) => {
+			const result = await OrderCollection.find({
+				email: req.params.email,
+			}).toArray();
+			res.send(result);
+			console.log("My myBookings Send Successfully", result);
+		});
+
+		// Get My myBookings
+		app.delete("/myBooking/:id", async (req, res) => {
+			const result = await OrderCollection.deleteOne({
+				_id: ObjectId(req.params.id),
+			});
+			res.send(result);
+			console.log("My myBookings Delete Successfully", result);
+		});
+
+		// Update Order Status
+		app.put("/bookingUpdate/:id", async (req, res) => {
+			const query = { _id: ObjectId(req.params.id) };
+			const options = { upsert: true };
+			const updateStatus = {
+				$set: {
+					status: "Approved",
+				},
+			};
+			const result = await OrderCollection.updateOne(
+				query,
+				updateStatus,
+				options
+			);
+			res.send(result);
+			console.log("Status Update Successfull", result);
+		});
+
+		// Get All Bookings
+		app.get("/allBookings", async (req, res) => {
+			const result = await OrderCollection.find({}).toArray();
+			res.send(result);
+			console.log("All Booking Send Successfully");
+		});
 
 		// Add Packeges
-		app.post("/packages", async (req, res) => {
-			const result = await packageCollection.insertOne(req.body);
+		app.post("/addpackage", async (req, res) => {
+			const result = await pakageCollection.insertOne(req.body);
 			res.send(result);
-			console.log(result);
+			console.log("One Pakage added Successfully");
 		});
 
 		// Get All Packeges
-		app.get("/allpakages", async (req, res) => {
-			const result = await packageCollection.find({}).toArray();
+		app.get("/allpackages", async (req, res) => {
+			const result = await pakageCollection.find({}).toArray();
 			res.send(result);
-			console.log("result");
+			console.log("All Packages Send Successfully");
+		});
+
+		// Get One Package
+		app.get("/package/:id", async (req, res) => {
+			const query = { _id: ObjectId(req.params.id) };
+			const result = await pakageCollection.findOne(query);
+			res.send(result);
+			console.log("One Package send Successfully");
+		});
+
+		// get search Pakage
+		app.get("/searchPakage", async (req, res) => {
+			const result = await pakageCollection
+				.find({
+					title: { $regex: req.query.search },
+				})
+				.toArray();
+			res.send(result);
+			console.log("Searched Package send Successfully");
 		});
 	} finally {
 		// await client.close()
